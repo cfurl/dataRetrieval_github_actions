6278e33b262ba508730274e1c7d4638e6f8c9c5f
cfurl <chad.furl@gmail.com>
2023-10-18 11:28:53 -0500

    com

--- make_data.R
+++ make_data.R
@@ -7,7 +7,7 @@ library(reactable)
 
 siteNo <- "08170000"
 pCode <- "00060"
-start.date <- Sys.Date()-10
+start.date <- Sys.Date()-20
 end.date <- Sys.Date()-1
 
 sm <- readNWISdv(siteNumbers = siteNo,
@@ -15,7 +15,7 @@ sm <- readNWISdv(siteNumbers = siteNo,
                  startDate = start.date,
                  endDate = end.date)
 
-date_seq<-as_tibble(seq(as.Date(start.date),by="day",length.out=11)) %>%
+date_seq<-as_tibble(seq(as.Date(start.date),by="day",length.out=21)) %>%
   set_names("Date") %>%
   left_join(sm) %>%
   set_names(c("Date", "agency", "site_no","discharge","qualifier"))
